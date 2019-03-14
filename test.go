d81717ac7be8b2cc2e259b25a40726e118cd7c1a
Omar Faroque (Anik) <faroque.ae@gmail.com>
2019-03-14 18:31:33 -0500

    json message publish on mqtt broker

--- test.go
+++ test.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/eclipse/paho.mqtt.golang"
-	"strconv"
+	"time"
 )
 
+type SensorData struct {
+	Name string
+	Body string
+	Time int64
+}
 func main()  {
 	done := make(chan bool)
 	opts := mqtt.NewClientOptions().AddBroker("tcp://localhost:1883").SetClientID("sample")
@@ -16,9 +22,14 @@ func main()  {
 	if token := c.Connect(); token.Wait() && token.Error() != nil {
 		panic(token.Error())
 	}
-	payload:="Hello World"
-	for i:=0;i<10 ;i++  {
-		if token := c.Publish("test/topic", 0, true, payload+strconv.Itoa(i)); token.Wait() && token.Error() != nil {
+	for i:=0;i<100 ;i++  {
+		m := SensorData{"Alice", "Hello", time.Now().UnixNano()}
+		b, err := json.Marshal(m)
+		if err!=nil{
+			panic(err)
+		}
+		fmt.Println(b)
+		if token := c.Publish("test/topic", 0, true, b); token.Wait() && token.Error() != nil {
 			fmt.Println(token.Error())
 		}
 		if token := c.Subscribe("test/topic", 0, msgRcvd); token.Wait() && token.Error() != nil {
@@ -30,6 +41,8 @@ func main()  {
 
 func msgRcvd(client mqtt.Client, message mqtt.Message) {
 	fmt.Print(client)
+	var m SensorData
+	err := json.Unmarshal(message.Payload(), &m)
+	if err!=nil{fmt.Print(err)}
 	fmt.Printf("Received message on topic: %s\nMessage: %s\n", message.Topic(), message.Payload())
-
 }
