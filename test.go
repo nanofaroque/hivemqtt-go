bfcdb62de1fe5e41f7b2fdde2efd50fcdd7f2357
Omar Faroque (Anik) <faroque.ae@gmail.com>
2019-03-14 15:48:30 -0500

    adding json to publish mqtt

--- test.go
+++ test.go
@@ -1,12 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/eclipse/paho.mqtt.golang"
-	"strconv"
 )
 
-func main()  {
+/*func main()  {
 	done := make(chan bool)
 	opts := mqtt.NewClientOptions().AddBroker("tcp://localhost:1883").SetClientID("sample")
 	opts.SetUsername("admin")
@@ -29,6 +29,49 @@ func main()  {
 }
 
 func msgRcvd(client mqtt.Client, message mqtt.Message) {
+	fmt.Print(client)
+	fmt.Printf("Received message on topic: %s\nMessage: %s\n", message.Topic(), message.Payload())
+
+}*/
+
+type SensorData struct {
+	temp  string `json:"sensor"`
+	pmcId string `json:"pmc_id"`
+	id    int32  `json:"id"`
+}
+
+func main() {
+	done := make(chan bool)
+	opts := mqtt.NewClientOptions().AddBroker("tcp://localhost:1883").SetClientID("sample")
+	opts.SetUsername("admin")
+	opts.SetPassword("hivemq")
+
+	c := mqtt.NewClient(opts)
+	if token := c.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+	payload := SensorData{
+		"55",
+		"starwood",
+		1,
+	}
+	for i := 0; i < 10; i++ {
+		fmt.Println(payload)
+		bytes, err := json.Marshal(payload)
+		if err != nil {
+			panic(err)
+		}
+		if token := c.Publish("json/topic", 0, true, bytes); token.Wait() && token.Error() != nil {
+			fmt.Println("Error: ",token.Error())
+		}
+		if token := c.Subscribe("json/topic", 0, jsonRcvd); token.Wait() && token.Error() != nil {
+			fmt.Println(token.Error())
+		}
+	}
+	<-done
+}
+
+func jsonRcvd(client mqtt.Client, message mqtt.Message) {
 	fmt.Print(client)
 	fmt.Printf("Received message on topic: %s\nMessage: %s\n", message.Topic(), message.Payload())
 
