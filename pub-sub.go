270b7dac12c5a3c4d71dbc03d98159fc603bc3a5
Omar Faroque (Anik) <faroque.ae@gmail.com>
2019-03-22 20:34:51 -0500

    minor change

--- pub-sub.go
+++ pub-sub.go
@@ -23,9 +23,9 @@ func main()  {
 		panic(token.Error())
 	}
 
-	if token := c.Subscribe("$share/group/test/topic", 1, msgRcvd); token.Wait() && token.Error() != nil {
+/*	if token := c.Subscribe("$share/group/test/topic", 1, msgRcvd); token.Wait() && token.Error() != nil {
 		fmt.Println(token.Error())
-	}
+	}*/
 	for i:=0;i<1000 ;i++  {
 		m := SensorData{"Alice", "Hello", time.Now().UnixNano()}
 		b, err := json.Marshal(m)
@@ -33,19 +33,18 @@ func main()  {
 			panic(err)
 		}
 		fmt.Println(b)
-		if token := c.Publish("test/topic", 1, true, b); token.Wait() && token.Error() != nil {
+		if token := c.Publish("test/topic", 2, true, b); token.Wait() && token.Error() != nil {
 			fmt.Println(token.Error())
 		}
 		time.Sleep(100 * time.Microsecond)
-
 	}
 	<-done
 }
-
+/*
 func msgRcvd(client mqtt.Client, message mqtt.Message) {
 	//fmt.Print(client)
 	var m SensorData
 	err := json.Unmarshal(message.Payload(), &m)
 	if err!=nil{fmt.Print(err)}
 	fmt.Printf("Received message on topic: %s\nMessage: %s\n", message.Topic(), message.Payload())
-}
+}*/
